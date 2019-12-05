4eb097ac045dc5af94ff8b58db64c05b007ba66c
Jose Carpio <jose.carpio@gmail.com>
2019-12-05 16:14:53 +0000

    tropes set films less than 10 tropes

--- tvtropes.pl
+++ tvtropes.pl
@@ -3,7 +3,7 @@
 # https://blog-en.openalfa.com/how-to-read-and-write-json-files-in-perl 
 
 use strict;
-use warnings;
+# use warnings;
  
 binmode STDOUT, ":utf8";
 use utf8;
@@ -14,24 +14,43 @@ my $DEBUG = 0;
 my $max_tropes = 10;
 
 my $json;
+
+my $file_name = $ARGV[0];
 {
   local $/; #Enable 'slurp' mode
-  open my $fh, "<", "tvtropes.json";
+  open my $fh,'<',$ARGV[0] or die "can't open file: $!";
+  # open my $fh, "<", "tvtropes.json";
   $json = <$fh>;
   close $fh;
 }
-my $data = decode_json($json);
 
-foreach my $key (keys %{$data}) {
-  my $film_data = $data->{$key};
+my $key;
+my $data = decode_json($json);
+my %tropes;
+my $film_data;
+
+foreach $key (keys %{$data}) {
+  $film_data = $data->{$key};
   my $num_tropes = scalar @{$film_data};
   if ($DEBUG) { print "film: $key num_tropes: $num_tropes\n"; }
-  if ($num_tropes <= $max_tropes) {
-     
-  } 
-
+  if ($num_tropes > $max_tropes) {
+     delete $data->{$key};
+  }
+  
+  foreach my $value (@{$film_data}) {
+     $tropes{$value}= "";
+  }
 }
 
+foreach $key (keys %tropes)
+{
+   print "$key\n";
+}
+
+
+open my $fh, ">", "$file_name" . "_less_than_10.json";
+print $fh encode_json($data);
+close $fh;
 # # Output to screen one of the values read
 # print "Boss' hobbies: " .
 #      $data->{'boss'}->{'Hobbies'}->[0] . "n";
