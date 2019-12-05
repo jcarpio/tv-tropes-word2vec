4022267b87f619e0ca65ef37143e40be450b5abd
Jose Carpio <jose.carpio@gmail.com>
2019-12-05 12:53:27 +0000

    obtained #tropes by film id 2

--- tvtropes.pl
+++ tvtropes.pl
@@ -1,16 +1,18 @@
 #!/usr/bin/perl
 #
+# https://blog-en.openalfa.com/how-to-read-and-write-json-files-in-perl 
+
 use strict;
-# use warnings;
+use warnings;
  
 binmode STDOUT, ":utf8";
 use utf8;
  
 use JSON;
 
-
+my $DEBUG = 0;
 my $max_tropes = 10;
- 
+
 my $json;
 {
   local $/; #Enable 'slurp' mode
@@ -23,6 +25,18 @@ my $data = decode_json($json);
 foreach my $key (keys %{$data}) {
   my $film_data = $data->{$key};
   my $num_tropes = scalar @{$film_data};
-  print "film: $key num_tropes: $num_tropes\n";
+  if ($DEBUG) { print "film: $key num_tropes: $num_tropes\n"; }
+  if ($num_tropes <= $max_tropes) {
+     
+  } 
+
 }
 
+# # Output to screen one of the values read
+# print "Boss' hobbies: " .
+#      $data->{'boss'}->{'Hobbies'}->[0] . "n";
+# Modify the value, and write the output file as json
+# $data->{'boss'}->{'Hobbies'}->[0] = "Swimming";
+# open my $fh, ">", "data_out.json";
+# print $fh encode_json($data);
+# close $fh;
