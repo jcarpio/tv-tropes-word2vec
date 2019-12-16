e2e5e01174a04c8c3a25f6671be79cad42ef1e6f
Jose Carpio <jose.carpio@gmail.com>
2019-12-16 09:36:53 +0000

    added more info to README.md

--- word2vec.py
+++ word2vec.py
@@ -1,12 +1,26 @@
 from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec, KeyedVectors
 
+# examples: https://rare-technologies.com/word2vec-tutorial/
+
 model =  KeyedVectors.load_word2vec_format('vectors.bin', binary= True)
 
 vector = model.wv['computer']
 
 print(vector)
 
-kk= model.most_similar(['girl', 'father'], ['boy'], topn=3)
+output = model.most_similar(['girl', 'father'], ['boy'], topn=3)
 
-print(kk)
+print(output)
+
+output = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+
+print(output)
+
+output = model.doesnt_match(['breakfast', 'cereal', 'dinner', 'lunch'])
+
+print(output)
+
+output = model.similarity('woman', 'man')
+
+print(output)
