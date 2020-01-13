8d309dbe6ff281323bbfa8e788a6719b5617a72c
Jose Carpio <jose.carpio@gmail.com>
2020-01-13 17:26:55 +0000

    tv-tropes-word2vec.py created to generate tropes's vector file

--- tv-tropes-word2vec.py
+++ tv-tropes-word2vec.py
@@ -3,31 +3,24 @@ from gensim.models import Word2Vec, KeyedVectors
 
 # examples: https://rare-technologies.com/word2vec-tutorial/
 
-model =  KeyedVectors.load_word2vec_format('ngrams_15_taken_9_vectors_add_film_name1.bin', binary= True)
+model =  KeyedVectors.load_word2vec_format('ngrams_15_taken_9_vectors.bin', binary= True)
 
-vector = model.wv['AStarIsBorn1976'] # film
+vector = model.wv['ElegantClassicalMusician']
 
 print(vector)
 
-# output = model.most_similar(['girl', 'father'], ['boy'], topn=3)
-output = model.most_similar(['AStarIsBorn1976', 'OldSchoolDogfight'], ['DreamLover'], topn=3)  # film trope film
+output = model.most_similar(['girl', 'father'], ['boy'], topn=3)
 
 print(output)
 
-# output = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
-
-output = model.most_similar(positive=['SourceMusic', 'LifeInAFishbowl'], negative=['CharacterDevelopment'], topn=1) #tope film trope
+output = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
 
 print(output)
 
-# output = model.doesnt_match(['breakfast', 'cereal', 'dinner', 'lunch'])
-
-output = model.doesnt_match(['Dinoshark', 'BeyondTheLineOfDuty', 'ReignOverMe', 'Fingore']) # last it's a trope
+output = model.doesnt_match(['breakfast', 'cereal', 'dinner', 'lunch'])
 
 print(output)
 
-# output = model.similarity('woman', 'man')
-
-output = model.similarity('ReignOverMe', 'MidwayToHeaven')  # two films
+output = model.similarity('woman', 'man')
 
 print(output)
